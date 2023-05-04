8d24ce63015fa841f070f1519ecb6c6ec966ab3a
dobryKum <zikovtm@gmail.com>
2023-05-04 12:40:43 +0200

    Initial Commit

---
+++ NewsApiTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApiTask
+//
+//  Created by Tsimafei Zykau on 4.05.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
